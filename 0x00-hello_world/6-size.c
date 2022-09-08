c9e4d1eaf469aaa26cd8b93d4d780bb83807f2d8
AdiyoFouad <odjfouad38@gmail.com>
2022-09-08 03:30:09 -0700

    First commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of a int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a float: %d byte(s)", sizeof(f));
 	return (0);
 }

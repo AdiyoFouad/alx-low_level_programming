ccbeb6c81d84a992f2b2d604566bd85f1e067bea
AdiyoFouad <odjfouad38@gmail.com>
2022-09-08 03:41:07 -0700

    First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-
 
 /**
  * main - Main function of my project
@@ -10,8 +8,6 @@
  */
 int main(void)
 {
-	char c[52];
-	strcpy(c, "\"Programming is like building a multilingual puzzle");
-	puts(c);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

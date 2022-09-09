ad8b8311a0d9bec014450086c4770808d380aa1c
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 04:08:41 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		if (i =! 101 && i =! 113)
+			putchar(i);
+	return (0);
+}

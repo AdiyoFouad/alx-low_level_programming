c8798de8784ce6edd68c4bd30291b6ec3b5ebb65
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 05:29:57 -0700

    Print combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if( i != 57 )
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(57);
+	putchar('\n');
 	return (0);
 }

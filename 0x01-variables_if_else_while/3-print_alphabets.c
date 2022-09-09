da140df22aa5fc4c152c10adeeeb0d8b588bc115
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 04:01:25 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i=97; i<123; i++)
+		putchar(i);
+	for (j=65; j<91; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}

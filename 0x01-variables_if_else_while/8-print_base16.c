71196e7c414817ffa3167539c11d26dde7ac6cd3
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 05:19:04 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 102; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

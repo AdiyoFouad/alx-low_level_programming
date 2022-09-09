477ef120b76b79e5a055bab07e515d3bbe257744
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 04:02:27 -0700

    Print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i=97; i<123; i++)
+	for (i = 97; i < 123; i++)
 		putchar(i);
-	for (j=65; j<91; j++)
+	for (j = 65; j < 91; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);

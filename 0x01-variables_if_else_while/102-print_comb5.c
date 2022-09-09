c3f33be3894794cbaaa2efb14f22bbe92805c968
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 07:47:51 -0700

    ok

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,7 +30,7 @@ int main(void)
 						putchar(' ');
 						putchar(k);
 						putchar(l);
-						if (!(i == 57 && j == 56 && k== 57 && l == 57))
+						if (!(i == 57 && j == 56 && k == 57 && l == 57))
 						{
 							putchar(',');
 							putchar(' ');

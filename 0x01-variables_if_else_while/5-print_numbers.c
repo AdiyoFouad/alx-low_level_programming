adc062fa321088fda0c97be159ee8517b9d8de7a
AdiyoFouad <odjfouad38@gmail.com>
2022-09-09 05:06:23 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ * */
+
+int main(void)
+{
+	int j;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

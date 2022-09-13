ec240ff909e33c60e4dea34d5b1412a574795d9f
AdiyoFouad <odjfouad38@gmail.com>
2022-09-13 07:28:13 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the 50 first fibonnacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int j;
+	long int k;
+	long int f;
+
+	j = 1;
+	k = 2;
+	printf("%ld, %ld, ", j, k);
+	for (i = 3; i <= 98; i++)
+	{
+		f = j + k;
+		printf("%ld", f);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		j = k;
+		k = f;
+	}
+	printf("\n");
+	return (0);
+}

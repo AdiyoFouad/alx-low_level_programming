d537942a1e72b6eb1e6c1e58b6ebfd91d0dd2a81
AdiyoFouad <odjfouad38@gmail.com>
2022-09-13 06:14:40 -0700

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * numberz - function that print number
+ * @n: number to print
+ * Return: void
+ */
+
+void numberz(int n)
+{
+	if (i / 100 != 0)
+	{
+		_putchar(i / 100 + '0');
+		_putchar(((i % 100) / 10 ) + '0');
+	}
+	if ((i / 100 == 0) && (i / 10 != 0))
+	{
+		_putchar(' ');
+		_putchar(i / 10 + '0');
+	}
+	_putchar(i % 10 + '0');
+}
+
+/**
+ * print_times_table - function that print the n times table, starting with 0
+ *@num: the paramater of the function
+ * Return: void
+ */
+
+void times_table(int num)
+{
+	int n;
+	int t;
+	int m;
+
+	for (n = 0; n <= num; n++)
+	{
+		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
+		for (t = 1; t <= num; t++)
+		{
+			m = n * t;
+			numberz(m);
+			if (t != num)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+

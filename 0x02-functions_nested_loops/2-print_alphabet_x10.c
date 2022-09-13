05a95ef9f68bfeb6c0cb60fb3a16cca4765c062b
AdiyoFouad <odjfouad38@gmail.com>
2022-09-13 02:41:11 -0700

    Prints lphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - function that prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - function that prints 10 times the alphabet in lowercase
  *
  * Return: void
  */

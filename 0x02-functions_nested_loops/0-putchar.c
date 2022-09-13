4967b83e332587e341a33158eeb54ff63c6e8fea
AdiyoFouad <odjfouad38@gmail.com>
2022-09-13 02:13:27 -0700

    Function, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * main - function to print
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	unsigned int c;
+
+	for (c = 0; c < sizeof(putchar); c++)
+	{
+		_putchar(putchar[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUED
+
+int _putchar(char);
+
+#endif

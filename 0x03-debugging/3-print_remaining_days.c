765e22849ecf7da19342a7566818267d8cb5ff55
AdiyoFouad <odjfouad38@gmail.com>
2022-09-14 01:07:31 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if ((year % 4 == 0 && year % 400 == 0) || !(year % 100 == 0))
 	{
-		if (month >= 3 && day >= 60)
+		if (month 2 >= 3 && day >= 60)
 		{
 			day++;
 		}
@@ -26,7 +26,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if(month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day-1, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{

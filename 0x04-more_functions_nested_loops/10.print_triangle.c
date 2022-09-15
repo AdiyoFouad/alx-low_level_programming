2aab3162677f62e0b9dc716d9c625728133be2c4
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 04:01:24 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/10.print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that draws a triangle on the terminal
+ * @size: size of te square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

43cdc7395bdd6db402a59f307d26fdb01ade1dc9
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 02:25:46 -0700

    Multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiples two integer
+ * @a: first integer
+ * @b: second integer
+ * Return: a * b
+ */
+
+int mul(int a, in b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 
 #endif

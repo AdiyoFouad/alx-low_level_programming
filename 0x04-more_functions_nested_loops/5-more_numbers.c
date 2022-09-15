279e66e92ac27ea05209be8152c4dfb412639881
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 03:02:14 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 != 0)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 
 #endif

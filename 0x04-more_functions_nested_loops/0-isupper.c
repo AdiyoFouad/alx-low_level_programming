fe64382c30cdeced85272d2aa56407e463e8937d
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 02:10:05 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - functions that check for uppercase character
+ * @c: the character
+ * Return: 1 if c is uppecase overwise 0
+ */
+
+int _isupper(int c)
+{
+	if (i >= 'A' || i <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}

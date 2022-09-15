0c9ba20f0c2910ba7892f63f14d32f0d5f7d6523
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 04:39:27 -0700

    Largest primr factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,10 +12,10 @@ int main(void)
 
 	n = 612852475143;
 	
-	for (i = 3; i < n; i = i + 2)
+	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
-			n /= i;
+			n = n / i;
 	}
 	printf("%lu\n", n);
 	return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,6 +12,7 @@ void print_line(int);
 void print_diagonal(int);
 void print_square(int);
 void print_triangle(int);
+void print_number(int);
 
 
 #endif

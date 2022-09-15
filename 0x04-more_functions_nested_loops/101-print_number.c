887869dc39238a2b7ca513dc39b436412441edba
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 05:00:33 -0700

    Print Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,18 +12,16 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n1 = -n; 
+		n1 = -n;
 		_putchar('-');
 	}
 	else
 	{
 		n1 = n;
 	}
-
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-
 	_putchar((n1 % 10) + '0');
 }

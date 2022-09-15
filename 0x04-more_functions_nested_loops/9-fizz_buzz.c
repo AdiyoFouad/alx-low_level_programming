628b2b2c9c026f0c45020ef4e7b9128f2557994a
AdiyoFouad <odjfouad38@gmail.com>
2022-09-15 03:55:19 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,8 @@ int main(void)
 {
 	int i;
 	
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -34,8 +35,9 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
+		i++;
 	}
 	return (0);
 }

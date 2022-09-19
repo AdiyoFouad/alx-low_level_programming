9bf171c238f8ebfef0a6367c70505f0026354c18
AdiyoFouad <odjfouad38@gmail.com>
2022-09-19 04:01:08 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string foolowed by new line to stdout
+ * @str: string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+	char c;
+
+	c = *str;
+	i = 0;
+	while (c)
+	{
+		_putchar(c);
+		i++;
+		c = *(str + i);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char);
 void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
+void _puts(char *);
 
 #endif

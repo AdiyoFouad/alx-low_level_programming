1f36f2df5d53c3a68ecc17abec9f931a4e983451
AdiyoFouad <odjfouad38@gmail.com>
2022-09-19 06:30:23 -0700

    strcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed by src, included the
+ * teminate null byte (\0), to the buffer pointed to by dest
+ * @dest: the first parameter
+ * @src: second prameter
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	i = 0;
+	while (*(src + i))
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		*(dest + i) = *(src + i);
+		i--;
+	}
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void rev_string(char *);
 void puts2(char *);
 void puts_half(char *);
 void print_array(int *,int);
+char *_strcpy(char *, char *);
 
 #endif

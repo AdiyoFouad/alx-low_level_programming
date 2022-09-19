de62fd8333af6587c9f659fbb916056eef81c98a
AdiyoFouad <odjfouad38@gmail.com>
2022-09-19 05:35:29 -0700

    Print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints a string foolowed by new line to stdout
+ * @str: string to print
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+	char c;
+
+	c = *str;
+	i = 0;
+	while (c)
+	{
+		i++;
+		c = *(str + i);
+	}
+	j = (i - 1) / 2;
+	while (j <= i)
+	{
+		_putchar(*(str + j));
+		j++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void _puts(char *);
 void print_rev(char *);
 void rev_string(char *);
 void puts2(char *);
+void puts_half(char *);
 
 #endif

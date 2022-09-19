164ebd4b3c1903092f1ba3dd108cb3fbbb7d2ed2
AdiyoFouad <odjfouad38@gmail.com>
2022-09-19 03:09:04 -0700

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int);
 int largest_number(int, int, int);
-void print_remaning_days(int, int, int);
+void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
 #endif

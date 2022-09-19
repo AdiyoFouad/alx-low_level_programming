56c9cd9117615640259722c68701942e74d7e35d
AdiyoFouad <odjfouad38@gmail.com>
2022-09-19 05:43:14 -0700

    Print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,8 +18,11 @@ void puts_half(char *str)
 		i++;
 		c = *(str + i);
 	}
-	j = (i - 1) / 2;
-	while (j <= i)
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+	while (j < i)
 	{
 		_putchar(*(str + j));
 		j++;

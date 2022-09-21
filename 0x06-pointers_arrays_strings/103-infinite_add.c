2fff5e6bbf3ad870fa42af10a56710d851502b3c
AdiyoFouad <odjfouad38@gmail.com>
2022-09-21 05:28:38 -0700

    Infinite dd

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * infinite_add - function that adds two numbers
+ * @a: first number
+ * @b: second number
+ * @r: buffer or result
+ * @size_r: buffer size
+ * Return: r or 0
+ */
+
+char *infinite_add(char *a, char *b, char *r, int size_r)
+{
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *cap_string(int *str);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int);
+char *infinite_add(char *, char *,char *, int);
 
 #endif

0f1bacd65fa5537f2a78187486ae02ea1e29f2af
AdiyoFouad <odjfouad38@gmail.com>
2022-09-22 08:54:36 -0700

    Capitalize string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ char *_strncpy(char *, char *, int);
 int _strcmp(char *, char *);
 void reverse_array(int *,int);
 char *string_toupper(char *);
-char *cap_string(int *str);
+char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int);

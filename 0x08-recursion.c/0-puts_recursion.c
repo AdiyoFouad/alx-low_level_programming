fc0af986b9fd8e99cc07faf6ab60099017b95a72
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 01:55:45 -0700

    Print string

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that print a string following by a new line
+ * @s: string to print
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (!s)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(s);
+		_puts_recursion(s + 1);
+	}
+}

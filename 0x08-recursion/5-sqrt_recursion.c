05a784e9c075c0a6ed561750a87ee9221bd4ba2b
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 02:52:36 -0700

    Natural root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural root of a number
+ * @n: parameter
+ * Return: natural root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int m;
+
+	if ( n < 0 )
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+	{
+		m = n--;
+		if (m * m = n && m >= 2)
+			return (m);
+		else
+			return (_sqrt_recursion(m));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@ void _print_rev_recursion(char *);
 int _strlen_recursion(char *);
 int factorial(int);
 int _pow_recursion(int, int);
+int _sqrt_recursion(int);
 
 #endif

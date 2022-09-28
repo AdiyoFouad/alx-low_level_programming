05efab01529d1cb248d40380ab25cb44cf4e9a28
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 04:55:38 -0700

    Compare string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 			return (0);
-		if (wildcmp(s1, s2 + 1) || wilcmp(s1 + 1, s2))
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
 	}
 	

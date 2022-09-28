aec40e07db51028790c50b06375b3b3ac6806aa1
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 02:14:52 -0700

    Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	_print_rev_recursion(s + 1);
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return;
-	}
-	else
-	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }

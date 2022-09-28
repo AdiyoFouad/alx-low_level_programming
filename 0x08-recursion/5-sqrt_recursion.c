6a9f34dbf82f87caa50f1b947192479abe22670a
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 03:07:47 -0700

    Natural root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * sqrt_check - verify if s is the square root of c
+ * @s: parameter
+ * @c: number to find sqrt of
+ * Return: -1 or sqrt of c
+ */
+
+int sqrt_check(int s, int c)
+{
+	if (s * s == c)
+		return (s);
+	if (s * s > c)
+		return (-1);
+	return (sqrt_check(s + 1, c));
+
+/**
  * _sqrt_recursion - function that returns the natural root of a number
  * @n: parameter
  * Return: natural root of n
@@ -16,10 +31,6 @@ int _sqrt_recursion(int n)
 		return (n);
 	else
 	{
-		m = n--;
-		if (m * m == n && m >= 2)
-			return (m);
-		else
-			return (_sqrt_recursion(m));
+		return (sqrt_check(1, n));
 	}
 }

26edbb36bc7778fefa95c72e592507f7bee8be4a
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 03:12:56 -0700

    Natural root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,11 +23,11 @@ int sqrt_check(int s, int c)
 
 int _sqrt_recursion(int n)
 {
-	if ( n < 0 )
+	if (n < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
+	if (n == 0 || n == 1)
 		return (n);
-	else
+	if (n > 1)
 	{
 		return (sqrt_check(1, n));
 	}

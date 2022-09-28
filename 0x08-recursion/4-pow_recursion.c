1534acd4c0377b9c7878deab09bbe5b2bc4e7959
AdiyoFouad <odjfouad38@gmail.com>
2022-09-28 02:41:16 -0700

    Power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,12 @@
  * Return: x power y
  */
 
-int _power_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
 	else
-		return (x * _power_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }

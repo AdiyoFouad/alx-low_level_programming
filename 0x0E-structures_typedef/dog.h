d020401aa8e0a655346356f7d22b343e5f1440d0
AdiyoFouad <odjfouad38@gmail.com>
2022-10-10 13:41:36 +0200

    Free dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,4 +23,5 @@ typedef struct dog dog_t;
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
 #endif

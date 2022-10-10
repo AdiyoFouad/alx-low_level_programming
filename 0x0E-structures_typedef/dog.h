b7aa9cc2490a724f7a6618d677f7fd6811a2fb47
AdiyoFouad <odjfouad38@gmail.com>
2022-10-10 12:56:51 +0200

    Type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_INCLUDED
+#define DOG_H_INCLUDED
+
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

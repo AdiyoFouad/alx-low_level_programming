34930cbd90ac52c29da480c44dcece99381259bf
AdiyoFouad <odjfouad38@gmail.com>
2022-10-10 13:29:50 +0200

    New type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 #endif

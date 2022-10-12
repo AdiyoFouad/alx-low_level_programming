2f46e91542463c397f0575cb902ecfba42866116
AdiyoFouad <odjfouad38@gmail.com>
2022-10-12 19:00:01 +0200

    Array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,6 @@
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *, size_t, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNCTION_POINTERS_H_ */

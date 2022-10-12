90842cffd9a19c5302cdd0f23af8943fc727fa7c
AdiyoFouad <odjfouad38@gmail.com>
2022-10-12 18:47:50 +0200

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stddef.h>
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H_ */

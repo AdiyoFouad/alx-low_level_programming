dc4327fa0d93bab478fce539fb9574e5ef7f88fc
AdiyoFouad <odjfouad38@gmail.com>
2022-10-13 16:17:52 +0200

    Sum them all

--- 0x08-recursion.c/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-int _putchar(char);
-void _puts_recursion(char *s);
-
-#endif
---
+++ 0x10-variadic_function/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+* sum_them_all - sum of all its parameters
+* @n: number of parameters
+* Return: the sum of all its parameters
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+	va_list ap;
+
+	if (n != 0)
+	{
+		va_start(ap, n);
+		while (n-- > 0)
+			sum += va_arg(ap, int);
+		va_end(ap);
+
+		return (sum);
+	}
+	return (n);
+}

9e01664eaf49ae9fc6266a2840af21b94c3acebb
AdiyoFouad <odjfouad38@gmail.com>
2022-10-20 19:10:50 +0200

    Add node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,7 @@ struct list_s *next;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void print(void) __attribute__((constructor));

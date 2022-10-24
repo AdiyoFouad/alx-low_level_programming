72eddc85e428acfbdcd20c5f1002f1d064022a70
AdiyoFouad <odjfouad38@gmail.com>
2022-10-24 18:17:11 +0200

    Print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t n  = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		n++;
 	}
-	return (count);
+	return (n);
 }

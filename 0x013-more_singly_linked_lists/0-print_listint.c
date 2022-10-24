354ef4d899f4f2fac85621b9b76700395cc0d829
AdiyoFouad <odjfouad38@gmail.com>
2022-10-24 18:05:23 +0200

    Print listint

--- 0x013-more_singly_linked_lists/0-print_listint.c
+++ 0x013-more_singly_linked_lists/0-print_listint.c
@@ -1,38 +1,18 @@
 #include "main.h"
-
-/**
- * print_number - print integers
- * @n: integer to print
- * Return: void
- */
-void print_number(int n)
-{
-	unsigned int num;
-
-	if (n < 0)
-	{
-		num = -n;
-		_putchar('-');
-	}
-	else
-		num = n;
-	if (num / 10)
-		print_number(num / 10);
-	_putchar(num % 10 + '0');
-}
+#include <stdio.h>
 
 /**
  * print_listin - prints all the element of a listint_t list
  * @h: list head
  * Return: the number of nodes
  */
- size_t print_listin(const listint_t *h)
+ size_t print_listint(const listint_t *h)
  {
     size_t n = 0;
 
     while (h != NULL)
     {
-        print_number(h->n);
+        printf("%d\n",h->n);
         h = h->next;
         n++
     }

7319a503d6defca2df018f202441e2236ce033e9
AdiyoFouad <odjfouad38@gmail.com>
2022-10-24 18:33:34 +0200

    Listint length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint -  prints all the elements of a listint_t list.
+ * listint_len -  prints all the elements of a listint_t list.
  * @h: list to print
  *
  * Return: number of elements in the list

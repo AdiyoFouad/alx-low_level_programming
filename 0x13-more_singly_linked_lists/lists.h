cbbc8e9434d13da0a422a69ea1e414f00dcc9811
AdiyoFouad <odjfouad38@gmail.com>
2022-10-25 18:55:32 +0200

    Free list int safe

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,5 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **, unsigned int, int);
 int delete_nodeint_at_index(listint_t **, unsigned int);
 listint_t *reverse_listint(listint_t **);
 size_t print_listint_safe(const listint_t *);
+size_t free_listint_safe(listint_t **);
 
 #endif

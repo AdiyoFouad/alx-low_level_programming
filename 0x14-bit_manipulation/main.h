931543405fbadc20a3b8d79997c9f23f3b23e851
AdiyoFouad <odjfouad38@gmail.com>
2022-10-27 15:28:33 +0200

    Clear bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,6 @@ unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
 
 #endif

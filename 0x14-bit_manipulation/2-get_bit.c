e6e8ebcb185730c1a863f12c055df178b87ca35a
AdiyoFouad <odjfouad38@gmail.com>
2022-10-27 15:26:06 +0200

    Get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*get_bit - returns the value of a bit at a given index.
+*@n: number.
+*@index: index of bit.
+*
+*Return: value of bit at index or -1 if error occurs.
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int v;
+
+	if (index > 64)
+		return (-1);
+	v = (n >> index) & 1;
+
+	return (v);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *);
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int);
+int get_bit(unsigned long int, unsigned int);
 
 #endif

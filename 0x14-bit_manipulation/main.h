722dff52d77fb146efd95edafc4b3d8f510a9294
AdiyoFouad <odjfouad38@gmail.com>
2022-10-27 15:31:36 +0200

    Get endianness of a machine

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
 int clear_bit(unsigned long int *, unsigned int);
 unsigned int flip_bits(unsigned long int, unsigned long int);
+int get_endianness();
 
 #endif
